4fc37e084ef27aafe3a7810d849a6d054adf07c8
Khusainov Khusniddin <xusniddin.ielts@gmail.com>
2022-12-15 21:12:07 +0500

    changed banner file names

--- src/wrappers/hero-slider/MainSlider.js
+++ src/wrappers/hero-slider/MainSlider.js
@@ -41,7 +41,7 @@ const MainSlider = () => {
   const sliderData = [
     {
       id: 'first-image',
-      image: `${process.env.PUBLIC_URL}/banners/desk1.jpg`,
+      image: `${process.env.PUBLIC_URL}/banners/desk2.jpg`,
       url: '/catalog',
       color: 'white',
       title: '',
@@ -71,7 +71,7 @@ const MainSlider = () => {
   const sliderDataMobile = [
     {
       id: 'first-image',
-      mobileImage: `${process.env.PUBLIC_URL}/banners/mob1.jpg`,
+      mobileImage: `${process.env.PUBLIC_URL}/banners/mob2.jpg`,
       url: '/catalog?filter_onSale=true',
       color: 'white',
       title: '',
