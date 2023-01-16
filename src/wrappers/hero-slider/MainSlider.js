15bb28e129fb79d6e79935248ce819724f1b47fd
Khusainov Khusniddin <xusniddin.ielts@gmail.com>
2023-01-16 17:59:53 +0500

    banner changed

--- src/wrappers/hero-slider/MainSlider.js
+++ src/wrappers/hero-slider/MainSlider.js
@@ -41,7 +41,7 @@ const MainSlider = () => {
   const sliderData = [
     {
       id: 'first-image',
-      image: `${process.env.PUBLIC_URL}/banners/desk4.png`,
+      image: `${process.env.PUBLIC_URL}/banners/desk5.jpg`,
       url: '/catalog',
       color: 'white',
       title: '',
@@ -71,7 +71,7 @@ const MainSlider = () => {
   const sliderDataMobile = [
     {
       id: 'first-image',
-      mobileImage: `${process.env.PUBLIC_URL}/banners/mob4.png`,
+      mobileImage: `${process.env.PUBLIC_URL}/banners/mob5.jpg`,
       url: '/catalog?filter_onSale=true',
       color: 'white',
       title: '',
