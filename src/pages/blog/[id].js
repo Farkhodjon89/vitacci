13e4ed6178ad1d33d92e5a6b479699ca6903bbbf
Farhod <ubaydullaevfarhod18@gmail.com>
2023-05-09 15:45:03 +0500

    rebuildos

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,17 +1,14 @@
-import client from '../../components/ApolloClient';
-import { POST_BY_ID, POSTS } from '../../../queries/posts';
-import React, { Fragment } from 'react';
-import { HeadData } from '../../components/Head';
-import LayoutOne from '../../components/Layout';
-import Breadcrumb from '../../wrappers/breadcrumb/Breadcrumb';
-import {
-  BreadcrumbsItem,
-  BreadcrumbsProvider,
-} from 'react-breadcrumbs-dynamic';
-import NotFoundComponent from '../../components/NotFound';
-import { StaticDataSingleton } from '../../utils/getStaticData';
-import striptags from 'striptags';
-import { formatPost } from '../../utils/functions';
+import client from '../../components/ApolloClient'
+import { POST_BY_ID, POSTS } from '../../../queries/posts'
+import React, { Fragment } from 'react'
+import { HeadData } from '../../components/Head'
+import LayoutOne from '../../components/Layout'
+import Breadcrumb from '../../wrappers/breadcrumb/Breadcrumb'
+import { BreadcrumbsItem, BreadcrumbsProvider } from 'react-breadcrumbs-dynamic'
+import NotFoundComponent from '../../components/NotFound'
+import { StaticDataSingleton } from '../../utils/getStaticData'
+import striptags from 'striptags'
+import { formatPost } from '../../utils/functions'
 
 const Post = ({ post, categories, notFound }) => {
   if (notFound || post == null) {
@@ -19,17 +16,17 @@ const Post = ({ post, categories, notFound }) => {
       <BreadcrumbsProvider>
         <NotFoundComponent />
       </BreadcrumbsProvider>
-    );
+    )
   }
 
   return (
     <BreadcrumbsProvider>
       <Fragment>
         <HeadData
-          pageUrl="/blog/"
+          pageUrl='/blog/'
           pageData={{
             postTitle: post.title,
-            image: post.featuredImage.node.sourceUrl,
+            image: post?.featuredImage?.node?.sourceUrl,
             date: post.rawDate,
           }}
           description={post.short}
@@ -38,21 +35,21 @@ const Post = ({ post, categories, notFound }) => {
         <BreadcrumbsItem to={'/'}>Главная</BreadcrumbsItem>
         <BreadcrumbsItem to={`/blog/${post.id}`}>{post.title}</BreadcrumbsItem>
 
-        <LayoutOne categories={categories} headerTop="visible">
+        <LayoutOne categories={categories} headerTop='visible'>
           {/* breadcrumb */}
           <Breadcrumb />
-          <div className="blog-area pt-100 pb-100">
-            <div className="container">
-              <div className="row">
-                <div className="col">
-                  <div className="blog-details-top">
-                    <div className="blog-details-img">
-                      <img alt="" src={post.featuredImage.node.sourceUrl} />
+          <div className='blog-area pt-100 pb-100'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col'>
+                  <div className='blog-details-top'>
+                    <div className='blog-details-img'>
+                      <img alt='' src={post?.featuredImage?.node?.sourceUrl} />
                     </div>
                   </div>
 
-                  <div className="px-2 blog-details-content">
-                    <div className="blog-meta-2">
+                  <div className='px-2 blog-details-content'>
+                    <div className='blog-meta-2'>
                       <ul>
                         <li>{post.date}</li>
                       </ul>
@@ -67,59 +64,59 @@ const Post = ({ post, categories, notFound }) => {
         </LayoutOne>
       </Fragment>
     </BreadcrumbsProvider>
-  );
-};
+  )
+}
 
 export const getStaticPaths = async () => {
-  const paths = [];
+  const paths = []
 
-  const fetchPosts = async after => {
+  const fetchPosts = async (after) => {
     const _tempPostsResult = await client.query({
       query: POSTS,
       variables: {
         first: 100,
         after,
       },
-    });
+    })
 
     paths.push(
-      ..._tempPostsResult.data.posts.nodes.map(post => ({
+      ..._tempPostsResult.data.posts.nodes.map((post) => ({
         params: { id: String(post.databaseId) },
       }))
-    );
+    )
 
     if (_tempPostsResult.data.posts.pageInfo.hasNextPage) {
-      await fetchPosts(_tempPostsResult.data.posts.pageInfo.endCursor);
+      await fetchPosts(_tempPostsResult.data.posts.pageInfo.endCursor)
     }
-  };
+  }
 
-  await fetchPosts();
+  await fetchPosts()
 
   return {
     paths,
     fallback: false,
-  };
-};
+  }
+}
 
 export const getStaticProps = async ({ params }) => {
-  const staticData = new StaticDataSingleton().getInstance();
-  await new StaticDataSingleton().checkAndFetch();
+  const staticData = new StaticDataSingleton().getInstance()
+  await new StaticDataSingleton().checkAndFetch()
 
-  let result;
+  let result
 
   try {
     result = await client.query({
       query: POST_BY_ID,
       variables: { id: params.id },
       fetchPolicy: 'no-cache',
-    });
+    })
   } catch (e) {
     return {
       props: {
         notFound: true,
       },
       revalidate: 600,
-    };
+    }
   }
 
   return {
@@ -129,7 +126,7 @@ export const getStaticProps = async ({ params }) => {
       notFound: false,
     },
     revalidate: 600,
-  };
-};
+  }
+}
 
-export default Post;
+export default Post
--- src/wrappers/blog/BlogPostsNoSidebar.js
+++ src/wrappers/blog/BlogPostsNoSidebar.js
@@ -1,22 +1,22 @@
-import React, { Fragment } from 'react';
-import Link from 'next/link';
+import React, { Fragment } from 'react'
+import Link from 'next/link'
 
 const BlogPostsNoSidebar = ({ posts }) => {
-  const postsComponents = posts.map(post => (
-    <div key={post.databaseId} className="col-lg-4 col-md-6 col-sm-12">
-      <div className="blog-wrap-2 mb-30">
-        <div className="blog-img-2">
+  const postsComponents = posts.map((post) => (
+    <div key={post.databaseId} className='col-lg-4 col-md-6 col-sm-12'>
+      <div className='blog-wrap-2 mb-30'>
+        <div className='blog-img-2'>
           <Link href={`/blog/${post.databaseId}`}>
             <a>
               <img
-                src={`${post.featuredImage.node.sourceUrl}`}
+                src={`${post?.featuredImage?.node?.sourceUrl}`}
                 alt={post.title}
               />
             </a>
           </Link>
         </div>
-        <div className="blog-content-2">
-          <div className="blog-meta-2">
+        <div className='blog-content-2'>
+          <div className='blog-meta-2'>
             <ul>
               <li>{post.date}</li>
             </ul>
@@ -27,8 +27,8 @@ const BlogPostsNoSidebar = ({ posts }) => {
             </Link>
           </h4>
           <div dangerouslySetInnerHTML={{ __html: post.short }} />
-          <div className="blog-share-comment">
-            <div className="blog-btn-2">
+          <div className='blog-share-comment'>
+            <div className='blog-btn-2'>
               <Link href={`/blog/${post.databaseId}`}>
                 <a>Читать дальше</a>
               </Link>
@@ -37,9 +37,9 @@ const BlogPostsNoSidebar = ({ posts }) => {
         </div>
       </div>
     </div>
-  ));
+  ))
 
-  return <Fragment>{postsComponents}</Fragment>;
-};
+  return <Fragment>{postsComponents}</Fragment>
+}
 
-export default BlogPostsNoSidebar;
+export default BlogPostsNoSidebar
