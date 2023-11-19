3d739415145ca3ae4c15a6c262f56c6260716cb0
Farhod <ubaydullaevfarhod18@gmail.com>
2023-11-19 12:07:40 +0500

    fix: urls

--- src/wrappers/ads-banner/index.js
+++ src/wrappers/ads-banner/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
 const AdsBanner = () => {
   return (
-    <div className="container pb-60">
-      <div className="row">
-        <div className="col-12 col-md-6 pb-4 pb-md-0 square-banner">
+    <div className='container pb-60'>
+      <div className='row'>
+        <div className='col-12 col-md-6 pb-4 pb-md-0 square-banner'>
           <img
-            className="w-100"
+            className='w-100'
             src={`${process.env.PUBLIC_URL}/banners/1_square.jpg`}
-            alt=""
+            alt=''
           />
-          <div className="url-button">
-            <Link href="/catalog/sumki">
-              <a className="animated def-button">Перейти</a>
+          <div className='url-button'>
+            <Link href='/catalog/sumki-zhenskij'>
+              <a className='animated def-button'>Перейти</a>
             </Link>
           </div>
         </div>
-        <div className="col-12 col-md-6 square-banner">
+        <div className='col-12 col-md-6 square-banner'>
           <img
-            className="w-100"
+            className='w-100'
             src={`${process.env.PUBLIC_URL}/banners/2_square.jpg`}
-            alt=""
+            alt=''
           />
-          <div className="url-button">
-            <Link href="/catalog/obuv">
-              <a className="animated def-button">Перейти</a>
+          <div className='url-button'>
+            <Link href='/catalog/obuv-zhenskij'>
+              <a className='animated def-button'>Перейти</a>
             </Link>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AdsBanner;
+export default AdsBanner
